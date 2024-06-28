bbec6a996dfbb6c322094588d400fcef8a83ae8e
irfanplazuardi <irfanplazuardi@gmail.com>
2024-06-28 18:04:34 +0700

    fix responsive laptop small

--- src/layout/Projects/index.jsx
+++ src/layout/Projects/index.jsx
@@ -12,7 +12,7 @@ const projects = [
       { name: "Material UI", imgSrc: "/materialui.png" },
     ],
     links: [
-      { type: "website", href: "https://dev--vogueschool-dev-env.netlify.app/", imgSrc: "/website.png", address: "vogueschool-dev-env.netlify.app" },
+      { type: "website", href: "https://dev--vogueschool-dev-env.netlify.app/", imgSrc: "/website.png", address: "vogueschool" },
       { type: "github", href: "https://github.com/irfanplazuardi/FinalProject_GroupH_FE", imgSrc: "/github.svg" },
     ],
     imgSrc: "/vogue-school.png",
@@ -44,13 +44,13 @@ const Projects = () => {
   return (
     <div className="bg-[url('/bg-projects.png')] bg-cover bg-center w-screen min-h-screen flex items-center justify-center relative" id="projects">
       <h1 className="text-5xl font-bold text-white absolute top-10 left-10">Checkout My Work Here</h1>
-      <div className="bg-green-700 bg-opacity-75 rounded-xl w-11/12 max-h-svh shadow-lg px-20 py-10 mt-20">
+      <div className="bg-green-700 bg-opacity-75 rounded-xl w-11/12 h-full shadow-lg px-20 py-10 mt-20">
         <Slider {...settings}>
           {projects.map((project, index) => (
             <div key={index} className="bg-white bg-opacity-50 rounded-xl shadow-lg h-[70vh]">
               <h3 className="text-6xl font-bold text-center p-1">{project.title}</h3>
-              <div className="flex justify-between gap-10 p-5">
-                <img src={project.imgSrc} alt={project.title} className="w-1/2 h-96" />
+              <div className="flex justify-between gap-10 p-5 ">
+                <img src={project.imgSrc} alt={project.title} className="w-1/2 h-fit lg:h-80" />
                 <div className="gap-5">
                   <h3 className="text-xl font-bold">Summary</h3>
                   <p className="text-justify font-light text-base pb-2">{project.summary}</p>
@@ -63,12 +63,12 @@ const Projects = () => {
                       </li>
                     ))}
                   </ul>
-                  <div className="flex gap-10 pt-10">
+                  <div className="flex gap-10 pt-10 lg:pt-2">
                     {project.links.map((link, idx) => (
-                      <div key={idx} className="flex gap-2 items-center">
+                      <div key={idx} className="flex gap-2 items-center lg:flex-col">
                         <img src={link.imgSrc} alt={`${project.title}-${link.type}`} className="w-8 h-8" />
                         <a href={link.href} target="_blank" rel="noopener noreferrer">
-                          {link.type === "website" ? `${link.address}` : "github.com/irfanplazuardi/"}
+                          {link.type === "website" ? `${link.address}` : "github.com/irfanplazuardi"}
                         </a>
                       </div>
                     ))}
